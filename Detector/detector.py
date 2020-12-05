6483361818e463faf664c7dde2260aad0c67ef5d
Elena <kolomeets.elena7@gmail.com>
2020-12-05 17:47:30 +0100

    Add Google reverse image search

--- Detector/detector.py
+++ Detector/detector.py
@@ -1,7 +1,10 @@
+from bs4 import BeautifulSoup
 import cv2
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
+import numpy
+import requests
 
 # check internet connection, access to camera, microphone?
 
@@ -10,32 +13,27 @@ def listen():
     """getting user's input with the microphone"""
     recognize = sr.Recognizer()
     # using Google Web Speech API because SpeechRecognition
-    # ships with a default API key for it so it's free
-    # BUT Google may revoke API, and
-    # there is a limit of 50 requests per day
+    # has its API key by default; BUT Google may revoke it;
+    # there is (apparently) limit of 50 requests per day
     mic = sr.Microphone()
     with mic as source:
-        # the user should wait 1 second while the audio
-        # is analyzed for ambient noises before talking
-        recognize.adjust_for_ambient_noise(source)
+        # audio is analyzed for ambient noises,talk after half second
+        recognize.adjust_for_ambient_noise(source, duration=0.5)
         audio = recognize.listen(source, phrase_time_limit=2)
         try:
             user_input = recognize.recognize_google(audio)
         except sr.RequestError:
             user_input = None
-            # send to the speaker later
             speaker("I could not connect to Internet to convert your speech to text.")
         except sr.UnknownValueError:
             user_input = None
-            # send to the speaker later
-            speaker("I did not understand what you just said. Could you repeat it please?")
+            speaker("I could not understand what you said.")
 
         return user_input
 
 
 def speaker(text):
     """returning results with the speaker"""
-    # for mobile would be best with Kivy?
     try:
         gTTS(text=text).save("speaker.mp3")
         playsound("speaker.mp3")
@@ -50,39 +48,50 @@ def capture_img():
     cap.release()
     if check:
         return frame
-
-
-def show_img(frame):
-    """displaying the image"""
-    cv2.imshow('capture', frame)
-    cv2.waitKey(0)
-    cv2.destroyWindow('capture')
+    else:
+        speaker("I didn't manage to take a picture")
 
 
 def save_img(frame):
     """saving the image in the file 'capture.png'"""
+    frame.dtype = numpy.uint8
     cv2.imwrite('capture.png', frame)
+
+
+def search(photo):
+    """performing Google reverse image search and returning the name of the captured object"""
+    url = "https://images.google.com/searchbyimage/upload"
+    file = {'encoded_image': (photo, open(photo, 'rb'))}
+    headers = {'User-agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 '
+                             'Safari/537.17'}
+    try:
+        # post request with a binary image file
+        response = requests.post(url=url, files=file, allow_redirects=False)
+        # get the search results page
+        photo_url = response.headers["Location"]
+        response1 = requests.get(photo_url, headers=headers)
+        all_results = BeautifulSoup(response1.text, "html.parser")
+        # fetch the result word(s) from the search line (next to image)
+        result = all_results.find("input", {"class": "gLFyf gsfi"})["value"]
+        speaker(f"There is probably {result} in front of you.")
+    except Exception:
+        speaker("I could not perform an image search.")
 
 
 def main():
     """
-    This Object Detector gets user's voice input (code word "sense")
-    and takes a photo with the device's back camera,
-    sends the photo to Google back image search
+    This Object Detector gets user's voice input (code word)
+    and takes a photo with the device's default camera,
+    sends the photo to Google reverse image search
     and returns audio output stating which object is in front of the user.
     """
     speech = listen()   # do we need other languages?
-    if speech is not None:
-        print(speech)
-        # speaker(speech)
     # word "sense" is not easily recognised; added similar words
-    # another option: other word, like "object" or "photo"/"take photo"
-    if speech in ["sense", "since", "send"]:
+    # other options: "object","photo","detect"; (I find "OBJECT" the best option)
+    if speech in ["sense", "since", "send", "sent", "sends", "object", "detect", "photo"]:
         photo = capture_img()
-        if photo is not None:
-            show_img(photo)
-        else:
-            speaker("I didn't manage to take a picture")
+        save_img(photo)
+        search("capture.png")
 
 
 if __name__ == '__main__':
