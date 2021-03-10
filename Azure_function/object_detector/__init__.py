3ec5970fe2029f87e0defd004f96b8489e3c25f1
Elena <kolomeets.elena7@gmail.com>
2021-03-10 15:12:17 +0100

    Add unit tests;replace - with _ in file names

--- Azure_function/object_detector/__init__.py
+++ Azure_function/object_detector/__init__.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 
 import azure.functions as func
 import requests
@@ -7,15 +7,19 @@ import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     """
-    TextReader Function or dweebs-eye function app;
+    ObjectDetector Function or dweebs-eye function app;
     gets an image from the request body and
     makes an API call to Azure Computer Vision API,
-    namely to OCR endpoint to get text from the image.
-    """
+    namely to Analyze endpoint to get an image description.
+    """    
     if req.method == 'POST':
         logging.info('Python HTTP trigger function processed a request.')
-        api_key = os.environ['CV_API_KEY1']
-        endpoint = os.environ['CV_ENDPOINT']
+        try:
+            api_key = os.environ['CV_API_KEY1']
+            endpoint = os.environ['CV_ENDPOINT']
+        except KeyError:
+            api_key = ''
+            endpoint = ''
 
         if req.headers.get('Content-Type') not in ['image/jpeg', 'image/png']:
             return func.HttpResponse('Not valid or not specified content type in the headers.', status_code=400)
@@ -23,28 +27,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             img = req.get_body()
             if not img or not isinstance(img, bytes):
                 return func.HttpResponse('No valid image provided.', status_code=400)
-        
-        req_url = endpoint + "vision/v3.1/ocr"
+
+        req_url = endpoint + "vision/v3.1/analyze"
         headers = {'Ocp-Apim-Subscription-Key': api_key,
                     'Content-Type': 'application/octet-stream'}
-        params = {'detectOrientation': 'true'}
+        params = {'visualFeatures': 'Description,Brands'}
 
         try:
             response = requests.post(req_url, headers=headers, params=params, data=img)
             response.raise_for_status()
             results = response.json()
-            result = ''
-            for region in results['regions']:
-                for line in region['lines']:
-                    for word in line['words']:
-                        result += word['text'] + ' '
-            if result:
-                if len(result) <= 150:
-                    result = 'The extracted text is: ' + result
-                else:
-                    result = 'The extracted text is too long.'
-            else:
-                result = 'No readable text found.'
+            description = results['description']['captions'][0]['text']
+            result = 'There is probably ' + description + ' in front of you.'
+            if results['brands']:
+                brand = results['brands'][0]['name']
+                result += ' The brand is ' + brand
             return func.HttpResponse(f'{result}')
         except:
             return func.HttpResponse('Could not perform image analysis.', status_code=400)
